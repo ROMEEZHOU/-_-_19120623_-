3b4deeb31253bf3da41b9791e717fd4e7c280135
ROMEEZHOU <yz7258@nyu.edu>
2022-08-23 10:59:08 +0800

    AbstractManager class added

---
+++ local_based/manager/AbstractManager.py
@@ -0,0 +1,31 @@
+#This is an abstract class for manager
+
+class AbstractManager():
+
+    def __init__(self):
+        None
+    
+    @abstractmethod
+    def run(self):
+        '''run a task under certain condition'''
+        pass
+
+    @abstractmethod
+    def stop(self):
+        '''stop a task under certain condition'''
+        pass
+
+    @abstractmethod
+    def check_alive(self):
+        '''regularly check if certain tasks are alive'''
+        pass
+
+    @abstractmethod
+    def detect_failure(self):
+        '''detect the failure of certain tasks'''
+        pass
+
+    @abstractmethod
+    def roll_back(self):
+        '''after a failure occurs, determine how to roll back and act roll-back'''
+        pass
